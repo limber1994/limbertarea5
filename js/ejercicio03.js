7d8105934822379570bfe5c6bcc343c8ba3c77f0
limber1994 <59769599+limber1994@users.noreply.github.com>
2020-09-09 21:13:12 -0500

    primera subida

---
+++ js/ejercicio03.js
@@ -0,0 +1,73 @@
+function libros(){
+    var lib = document.getElementsByClassName("list-group-item");
+    console.log(lib);
+    lib[0].innerHTML="Meg";
+    lib[1].innerHTML="Agua para elefantes";
+    lib[2].innerHTML="Oscura";
+    lib[3].innerHTML="Plenitud";
+    lib[4].innerHTML="Testamenyo Maya";
+}
+function peliculas(){
+    var lib = document.getElementsByClassName("list-group-item");
+    console.log(lib);
+    lib[0].innerHTML="Gladiador";
+    lib[1].innerHTML="Titanic";
+    lib[2].innerHTML="X-Men";
+    lib[3].innerHTML="Avengers";
+    lib[4].innerHTML="Rey Leon";
+}
+function musica(){
+    var lib = document.getElementsByClassName("list-group-item");
+    console.log(lib);
+    lib[0].innerHTML="Pablo Alboran";
+    lib[1].innerHTML="Laura Pusini";
+    lib[2].innerHTML="Robbie Willians";
+    lib[3].innerHTML="Katy Perry";
+    lib[4].innerHTML="Coldplay";
+}
+function libro_1(){
+    var a = document.getElementsByTagName("img");
+    a[0].src='imagen/libros/libros1.jpg';
+    a[0].height=300;
+    a[0].width=300;
+    a[1].src='imagen/libros/libros2.jpg';
+    a[1].height=300;
+    a[1].width=300;
+    a[2].src='imagen/libros/libros3.jpg';
+    a[2].height=300;
+    a[2].width=300;
+    a[3].src='imagen/libros/libros4.jpg';
+    a[3].height=300;
+    a[3].width=300;
+}
+function peliculas1(){
+    var a = document.getElementsByTagName("img");
+    a[0].src='imagen/peliculas/peliculas1.jpg';
+    a[0].height=300;
+    a[0].width=300;
+    a[1].src='imagen/peliculas/peliculas2.jpg';
+    a[1].height=300;
+    a[1].width=300;
+    a[2].src='imagen/peliculas/peliculas3.jpg';
+    a[2].height=300;
+    a[2].width=300;
+    a[3].src='imagen/peliculas/peliculas4.jpg';
+    a[3].height=300;
+    a[3].width=300;
+}
+function musica1(){
+    var a = document.getElementsByTagName("img");
+    a[0].src='imagen/musica/musica1.jpg';
+    a[0].height=300;
+    a[0].width=300;
+    a[1].src='imagen/musica/musica2.jpg';
+    a[1].height=300;
+    a[1].width=300;
+    a[2].src='imagen/musica/musica3.jpg';
+    a[2].height=300;
+    a[2].width=300;
+    a[3].src='imagen/musica/musica4.jpg';
+    a[3].height=300;
+    a[3].width=300;
+}
+
---
+++ js/ejercicio04.js
@@ -0,0 +1,448 @@
+function sumar(){
+    var a = document.getElementById("n1").value;
+    var b = document.getElementById("n2").value;
+    var c = document.getElementById("resp");
+    c.value=parseInt(a)+parseInt(b);
+}
+function restar(){
+    var a = document.getElementById("n1").value;
+    var b = document.getElementById("n2").value;
+    var c = document.getElementById("resp");
+    c.value=parseInt(a)-parseInt(b);
+}
+function multiplicar(){
+    var a = document.getElementById("n1").value;
+    var b = document.getElementById("n2").value;
+    var c = document.getElementById("resp");
+    c.value=parseInt(a)*parseInt(b);
+}
+function dividir(){
+    var a = document.getElementById("n1").value;
+    var b = document.getElementById("n2").value;
+    var c = document.getElementById("resp");
+    c.value=parseInt(a)/parseInt(b);
+}
+function sumaR(){
+    var x,y;
+    var z;
+    var resultado;
+    var a = document.getElementById("r1").value;
+    var b = document.getElementById("r2").value;
+    switch(a){
+        case "I":
+             x=1;
+            break;
+        case "II":
+             x=2;
+            break;
+        case "III":
+            x=3;
+            break;
+        case "IV":
+             x=4;
+            break;
+        case "V":
+             x=5;
+            break;
+        case "VI":
+             x=6;
+            break;
+        case "VII":
+             x=7;
+            break;
+        case "VIII":
+             x=8;
+            break;
+        case "IX":
+             x=9;
+            break;
+        case "X":
+             x=10;
+            break;
+    }
+    switch(b){
+        case "I":
+             y=1;
+            break;
+        case "II":
+            y=2;
+            break;
+        case "III":
+             y=3;
+            break;
+        case "IV":
+             y=4;
+            break;
+        case "V":
+             y=5;
+            break;
+        case "VI":
+             y=6;
+            break;
+        case "VII":
+             y=7;
+            break;
+        case "VIII":
+             y=8;
+            break;
+        case "IX":
+             y=9;
+            break;
+        case "X":
+             y=10;
+            break;
+    }
+    var c = document.getElementById("respR");
+    resultado=x+y;
+    switch(resultado){
+        case 1:
+            z="I";
+           break;
+       case 2:
+           z="II";
+           break;
+       case 3:
+            z="III";
+           break;
+       case 4:
+            z="IV";
+           break;
+       case 5:
+            z="V";
+           break;
+       case 6:
+            z="VI";
+           break;
+       case 7:
+            z="VII";
+           break;
+       case 8:
+            z="VII";
+           break;
+       case 9:
+            z="IX";
+           break;
+       case 10:
+            z="X";
+           break;
+    }
+    c.value=z;
+}
+function retarR(){
+    var x,y;
+    var z;
+    var resultado;
+    var a = document.getElementById("r1").value;
+    var b = document.getElementById("r2").value;
+    switch(a){
+        case "I":
+             x=1;
+            break;
+        case "II":
+             x=2;
+            break;
+        case "III":
+            x=3;
+            break;
+        case "IV":
+             x=4;
+            break;
+        case "V":
+             x=5;
+            break;
+        case "VI":
+             x=6;
+            break;
+        case "VII":
+             x=7;
+            break;
+        case "VIII":
+             x=8;
+            break;
+        case "IX":
+             x=9;
+            break;
+        case "X":
+             x=10;
+            break;
+    }
+    switch(b){
+        case "I":
+             y=1;
+            break;
+        case "II":
+            y=2;
+            break;
+        case "III":
+             y=3;
+            break;
+        case "IV":
+             y=4;
+            break;
+        case "V":
+             y=5;
+            break;
+        case "VI":
+             y=6;
+            break;
+        case "VII":
+             y=7;
+            break;
+        case "VIII":
+             y=8;
+            break;
+        case "IX":
+             y=9;
+            break;
+        case "X":
+             y=10;
+            break;
+    }
+    var c = document.getElementById("respR");
+    resultado=x-y;
+    switch(resultado){
+        case 1:
+            z="I";
+           break;
+       case 2:
+           z="II";
+           break;
+       case 3:
+            z="III";
+           break;
+       case 4:
+            z="IV";
+           break;
+       case 5:
+            z="V";
+           break;
+       case 6:
+            z="VI";
+           break;
+       case 7:
+            z="VII";
+           break;
+       case 8:
+            z="VII";
+           break;
+       case 9:
+            z="IX";
+           break;
+       case 10:
+            z="X";
+           break;
+    }
+    c.value=z;
+}
+function multiplicarR(){
+    var x,y;
+    var z;
+    var resultado;
+    var a = document.getElementById("r1").value;
+    var b = document.getElementById("r2").value;
+    switch(a){
+        case "I":
+             x=1;
+            break;
+        case "II":
+             x=2;
+            break;
+        case "III":
+            x=3;
+            break;
+        case "IV":
+             x=4;
+            break;
+        case "V":
+             x=5;
+            break;
+        case "VI":
+             x=6;
+            break;
+        case "VII":
+             x=7;
+            break;
+        case "VIII":
+             x=8;
+            break;
+        case "IX":
+             x=9;
+            break;
+        case "X":
+             x=10;
+            break;
+    }
+    switch(b){
+        case "I":
+             y=1;
+            break;
+        case "II":
+            y=2;
+            break;
+        case "III":
+             y=3;
+            break;
+        case "IV":
+             y=4;
+            break;
+        case "V":
+             y=5;
+            break;
+        case "VI":
+             y=6;
+            break;
+        case "VII":
+             y=7;
+            break;
+        case "VIII":
+             y=8;
+            break;
+        case "IX":
+             y=9;
+            break;
+        case "X":
+             y=10;
+            break;
+    }
+    var c = document.getElementById("respR");
+    resultado=x*y;
+    switch(resultado){
+        case 1:
+            z="I";
+           break;
+       case 2:
+           z="II";
+           break;
+       case 3:
+            z="III";
+           break;
+       case 4:
+            z="IV";
+           break;
+       case 5:
+            z="V";
+           break;
+       case 6:
+            z="VI";
+           break;
+       case 7:
+            z="VII";
+           break;
+       case 8:
+            z="VIII";
+           break;
+       case 9:
+            z="IX";
+           break;
+       case 10:
+            z="X";
+           break;
+    }
+    c.value=z;
+}
+function dividirR(){
+    var x,y;
+    var z;
+    var resultado;
+    var a = document.getElementById("r1").value;
+    var b = document.getElementById("r2").value;
+    switch(a){
+        case "I":
+             x=1;
+            break;
+        case "II":
+             x=2;
+            break;
+        case "III":
+            x=3;
+            break;
+        case "IV":
+             x=4;
+            break;
+        case "V":
+             x=5;
+            break;
+        case "VI":
+             x=6;
+            break;
+        case "VII":
+             x=7;
+            break;
+        case "VIII":
+             x=8;
+            break;
+        case "IX":
+             x=9;
+            break;
+        case "X":
+             x=10;
+            break;
+    }
+    switch(b){
+        case "I":
+             y=1;
+            break;
+        case "II":
+            y=2;
+            break;
+        case "III":
+             y=3;
+            break;
+        case "IV":
+             y=4;
+            break;
+        case "V":
+             y=5;
+            break;
+        case "VI":
+             y=6;
+            break;
+        case "VII":
+             y=7;
+            break;
+        case "VIII":
+             y=8;
+            break;
+        case "IX":
+             y=9;
+            break;
+        case "X":
+             y=10;
+            break;
+    }
+    var c = document.getElementById("respR");
+    resultado=x/y;
+    switch(resultado){
+        case 1:
+            z="I";
+           break;
+       case 2:
+           z="II";
+           break;
+       case 3:
+            z="III";
+           break;
+       case 4:
+            z="IV";
+           break;
+       case 5:
+            z="V";
+           break;
+       case 6:
+            z="VI";
+           break;
+       case 7:
+            z="VII";
+           break;
+       case 8:
+            z="VII";
+           break;
+       case 9:
+            z="IX";
+           break;
+       case 10:
+            z="X";
+           break;
+    }
+    c.value=z;
+}
